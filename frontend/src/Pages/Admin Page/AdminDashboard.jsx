bfaad7edcc56966886445ba60159a339346264b5
Fadhlan Khusnaini Al Barid <114384445+aXLan666@users.noreply.github.com>
2023-08-07 06:20:20 +0700

    Update AdminDashboard.jsx
    Conect component PelatihanSaya

--- frontend/src/Pages/Admin Page/AdminDashboard.jsx
+++ frontend/src/Pages/Admin Page/AdminDashboard.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import NavSideBar from "../../components/Admin/Sidebar";
 import Dashboard from '../../components/Admin/Dashboard/Dashboard';
-import Dashboard from '../../components/Admin/Dashboard/Dashboard';
+import PelatihanSaya from '../../components/Admin/PelatihanSaya/PelatihanSaya';
 
 function AdminDashboard() {
   return (
@@ -12,7 +12,7 @@ function AdminDashboard() {
         </Routes>
         <Routes>
           <Route path='/Admin/Dashboard' element={<Dashboard />} />
-          
+          <Route path='/Admin/Pelatihan' element={<PelatihanSaya />} />
         </Routes>
       </div>
     </>
