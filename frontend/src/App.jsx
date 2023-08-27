664e1e94e4c818f1f080d117eb374d19091e27a2
BintangLuthfian <wortelmobile@gmail.com>
2023-08-27 22:31:46 +0700

    Perbaikan

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@
 
 // import './App.css';
 // // import Home from "./Pages/Home";
-import AdminDashboard from './Pages/Admin Page/AdminDashboard';
+// import AdminDashboard from './Pages/Admin Page/AdminDashboard';
 // // import KategoriPelatihan from './Pages/KategoriPelatihan';
 // // import Login from './Pages/Login';
 // // import Register from './Pages/Register';
@@ -43,7 +43,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import 'flowbite';
 
-// users
+                    // users
 
 // import Home from "./Pages/Home";
 // import Dashboard from "./Pages/Dashboard";
@@ -58,67 +58,65 @@ import 'flowbite';
 // import Payment from "./Pages/PaymentHistory/Payment";
 // import Profil from "./Pages/Profil";
 
-// admin 
+                    // admin 
 
 
-// import Users from './components/Admin/User/Users';
-// import AddUser from './components/Admin/User/AddUser';
-// import EditUser from './components/Admin/User/EditUser';
-// import NavSideBar from "./components/Admin/Sidebar";
-// import Dashboard from './components/Admin/Dashboard/Dashboard';
-// import PelatihanSaya from './components/Admin/PelatihanSaya/PelatihanSaya';
-// import Materi from './components/Admin/PelatihanSaya/Materi';
-// import Detail from './components/Admin/PelatihanSaya/Detail';
-// import RiwayatKuis from './components/Admin/PelatihanSaya/RiwayatKuis';
-// import Sertifikat from './components/Admin/Sertifikat/Sertifikat';
-// import PaymentHistory from './components/Admin/PaymentHistory/PaymentHistory';
+import Users from './components/Admin/User/Users';
+import AddUser from './components/Admin/User/AddUser';
+import EditUser from './components/Admin/User/EditUser';
+import NavSideBar from "./components/Admin/Sidebar";
+import Dashboard from './components/Admin/Dashboard/Dashboard';
+import PelatihanSaya from './components/Admin/PelatihanSaya/PelatihanSaya';
+import Materi from './components/Admin/PelatihanSaya/Materi';
+import Detail from './components/Admin/PelatihanSaya/Detail';
+import RiwayatKuis from './components/Admin/PelatihanSaya/RiwayatKuis';
+import Sertifikat from './components/Admin/Sertifikat/Sertifikat';
+import PaymentHistory from './components/Admin/PaymentHistory/PaymentHistory';
+
 
 function App() {
   return (
+    <BrowserRouter>
+    <Routes>
+      
+         {/* User */}
 
-    <AdminDashboard />
+       {/* <Route path="/" element={<Home />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/kategori" element={<KategoriPelatihan />} />
+      <Route path="/hal-pembayaran" element={<Data />} />
+      <Route path="/bank" element={<Ban />} />
+      <Route path="/E-wallet" element={<Ewall />} />
+      <Route path="/credit-card" element={<Cred />} />
+      <Route path="/pusat-pengetahuan" element={<PusatPengetahuan2 />} />
+      <Route path="/payment-history" element={<Payment />} />
+      <Route path="/profil" element={<Profil />} /> */}
 
-    // <BrowserRouter>
-    // <Routes>
-
-    //      {/* User */}
-
-    //    <Route path="/" element={<Home />} />
-    //   <Route path="/dashboard" element={<Dashboard />} />
-    //   <Route path="/login" element={<Login />} />
-    //   <Route path="/register" element={<Register />} />
-    //   <Route path="/kategori" element={<KategoriPelatihan />} />
-    //   <Route path="/hal-pembayaran" element={<Data />} />
-    //   <Route path="/bank" element={<Ban />} />
-    //   <Route path="/E-wallet" element={<Ewall />} />
-    //   <Route path="/credit-card" element={<Cred />} />
-    //   <Route path="/pusat-pengetahuan" element={<PusatPengetahuan2 />} />
-    //   <Route path="/payment-history" element={<Payment />} />
-    //   <Route path="/profil" element={<Profil />} />
-
-    //      {/* admin */}
-
-    //    {/* <Route path="/admin/*" element={<NavSideBar />} />
-    //      </Routes>
+         {/* admin */}
+        
+       <Route path="/admin/*" element={<NavSideBar />} />
+         </Routes>
 
 
-    //    <Routes>
+       <Routes>
 
-    //   <Route path='/admin/Dashboard' element={<Dashboard />} />
-    //   <Route path='/admin/Pelatihan' element={<PelatihanSaya />} />
-    //   <Route path='/admin/Pelatihan/Materi' element={<Materi />} />
-    //   <Route path='/admin/Pelatihan/Detail' element={<Detail />} />
-    //   <Route path='/admin/Dashboard/Kuis' element={<RiwayatKuis />} />
-    //   <Route path='/admin/Sertifikat' element={<Sertifikat />} />
-    //   <Route path='/admin/PaymentHistory' element={<PaymentHistory />} />
-    //   </Routes>
+      <Route path='/admin/Dashboard' element={<Dashboard />} />
+      <Route path='/admin/Pelatihan' element={<PelatihanSaya />} />
+      <Route path='/admin/Pelatihan/Materi' element={<Materi />} />
+      <Route path='/admin/Pelatihan/Detail' element={<Detail />} />
+      <Route path='/admin/Dashboard/Kuis' element={<RiwayatKuis />} />
+      <Route path='/admin/Sertifikat' element={<Sertifikat />} />
+      <Route path='/admin/PaymentHistory' element={<PaymentHistory />} />
+      </Routes>
 
-    //         <Routes>
-    //           <Route path='/admin/Users' element={<Users />} /> */}
+            <Routes>
+              <Route path='/admin/Users' element={<Users />} />
 
 
-    //   </Routes>
-    // </BrowserRouter>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
